9ac97407b26202e6b2074230a30e970c9515246c
Ilya Filatov <filil2003@yandex.ru>
2024-10-11 13:38:08 +0300

    Enable support for environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
+import { env } from 'node:process';
 import express, { type Express, type Request, type Response } from 'express';
 
 const app: Express = express();
-const PORT = 3000;
+const HOST = env['SERVER_HOST'];
+const PORT = env['SERVER_PORT'];
 
 app.get('/', (_req: Request, res: Response): void => {
   res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
