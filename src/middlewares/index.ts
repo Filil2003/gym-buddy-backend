8eca5ba6e7e8e886ce5d9f8a511bd74317246d99
Ilya Filatov <filil2003@yandex.ru>
2024-10-21 22:03:24 +0300

    Fix: add missing re-export

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,4 @@
+export { errorHandlerMiddleware } from './error-handler.middleware.js';
 export { httpResponseLoggerMiddleware } from './http-response-logger.middleware.js';
 export { pageNotFoundMiddleware } from './page-not-found.middleware.js';
 export { responseModifierMiddleware } from './response-modifier.middleware.js';
