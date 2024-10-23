8b9f92a540a7bb2e8be8bd5e1f10ba9e47539e66
Ilya Filatov <filil2003@yandex.ru>
2024-10-23 13:56:53 +0300

    Remove unnecessary comments

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -3,19 +3,14 @@ import { Router } from 'express';
 
 const userRouter = Router();
 
-// GET /users
 userRouter.get('/users', userController.getAllUsers);
 
-// GET /users/:id
 userRouter.get('/users/:id', userController.getUserById);
 
-// POST /users
 userRouter.post('/users', userController.createUser);
 
-// PUT /users/:id
 userRouter.put('/users/:id', userController.updateUser);
 
-// DELETE /users/:id
 userRouter.delete('/users/:id', userController.deleteUser);
 
 export { userRouter };
